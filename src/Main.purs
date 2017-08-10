fcaf569c4008583da3185b3ea668e3ebed10c0e0
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-08-10 17:42:19 -0400

    Still figuring out how to do a setTimeout

--- src/Main.purs
+++ src/Main.purs
@@ -1,19 +1,20 @@
 module Main where
 
 import Control.Monad.Aff (delay)
-import Control.Monad.Aff.Console (CONSOLE, logShow)
+import Control.Monad.Aff.Console (CONSOLE)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Random (RANDOM)
+import DOM (DOM)
+import Data.Array (fromFoldable)
 import Data.Helpers (generateSequence, convertToColors)
-import Data.List (List(..), snoc)
-import Data.Maybe (Maybe(..))
+import Data.List (List(..), snoc, index, range)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Sounds (play, SOUND)
 import Data.Styles (buttonStyled)
 import Data.Time.Duration (Milliseconds(..))
-import Data.Sounds (play, SOUND)
-import DOM (DOM)
-import Prelude hiding (div, id)
-import Pux (CoreEffects, EffModel, noEffects, start)
+import Prelude hiding (div,id)
+import Pux (CoreEffects, EffModel, noEffects, onlyEffects, start)
 import Pux.DOM.Events (DOMEvent, onChange, onClick)
 import Pux.DOM.HTML (HTML)
 import Pux.Renderer.React (renderToDOM)
@@ -23,10 +24,12 @@ import Text.Smolder.Markup (text, (#!), (!), (!?))
 
 
 data Event 
-  = Start 
-  | NewSequence (List String) 
-  | UserClick String 
-  | ResetColor 
+  = Start
+  | AnimateColor Number String
+  | NewSequence (List String)
+  | PlaySequence
+  | UserClick String
+  | ResetColor
   | Strict DOMEvent
 
 type AppEffects = (random :: RANDOM, console :: CONSOLE, dom :: DOM, sound :: SOUND)
@@ -48,28 +51,50 @@ init =
   , strict: false
   }
 
+generatePlaySequence :: forall e. Applicative e => Int -> List String -> Array (e (Maybe Event))
+generatePlaySequence count sequence =
+  range 0 (count - 1) #
+  map (\v -> pure $ Just $ AnimateColor 300.0 (fromMaybe "" $ index sequence v) ) #
+  fromFoldable
+
 foldp :: Event -> State -> EffModel State Event AppEffects
 foldp Start state = 
-  { state: state
-  , effects: [ do
-      result <- liftEff generateSequence
-      let colors = convertToColors result
-      pure $ Just $ NewSequence colors
-    ]
-  }
-foldp (NewSequence list) state = 
-  { state: state { sequence = list }
-  , effects: [ logShow list *> pure Nothing ]
-  }
-foldp (UserClick color) state = 
-  { state: state { userInput = snoc state.userInput color, currentColor = color }
+  if state.count > 0 then
+    noEffects $ state
+  else 
+  { state: state { count = state.count + 1 }
   , effects: 
-      [ logShow (snoc state.userInput color) *> pure Nothing
-      , liftEff $ play color *> pure Nothing
-      , (delay $ Milliseconds 300.0) *> (pure $ Just $ ResetColor)
-    ]
+      [ do
+          result <- liftEff generateSequence
+          let colors = convertToColors result
+          pure $ Just $ NewSequence colors
+      ]
   }
-foldp ResetColor state = noEffects $ state { currentColor = "" } 
+
+foldp (NewSequence list) state =
+  { state: state { sequence = list }
+  , effects: [pure $ Just $ PlaySequence]
+  }
+
+foldp (UserClick color) state =
+  { state: state { userInput = snoc state.userInput color, currentColor = color }
+  , effects: [ pure $ Just $ AnimateColor 300.0 color ]
+  }
+foldp ResetColor state = noEffects $ state { currentColor = "" }
+
+foldp (AnimateColor time color) state =
+  { state: state { currentColor = color },
+    effects: 
+      [ liftEff $ play color *> pure Nothing
+      , do 
+        delay $ Milliseconds time
+        pure $ Just $ ResetColor
+      ]
+  }
+
+foldp PlaySequence state =
+  onlyEffects state $ generatePlaySequence state.count state.sequence
+
 foldp (Strict e) state = noEffects $ state { strict = not state.strict }
 
 
@@ -93,6 +118,8 @@ view state =
         ! buttonStyled "blue" state.currentColor
         #! onClick (const $ UserClick "blue")
         $ text ""
+    div do
+      text $ "Count" <> show state.count
     div do
       label $ text "Strict"
       (input !? state.strict) (checked "checked") ! type' "checkbox" #! onChange Strict 
